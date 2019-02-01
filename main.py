a37253c2ec6cb465b8b2f8c55a2dfd32889116fa
doctorblinch <docblinch@gmail.com>
2019-02-01 14:57:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,47 +3,52 @@ flash, get_flashed_messages, url_for
 from models import InForm, split_into_sentences, get_sorted_messages
 
 
-#Initialization of Flask app.
+# Initialization of Flask app.
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'any secret string'
 _text = ''
 
 # Part that is responsible for different URLs
 
-# In this function we create a form for user to input the text
-# that we are going to sort.
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """
+    In this function we create a form for user to input the text
+    that we are going to sort.
+    """
     global _text
     form = InForm()
     if form.validate_on_submit():
         _text = form.text.data
-    #    flash(form.text.data)
         return redirect(url_for('choose'))
-    return render_template('home.html', form = form)
+    return render_template('home.html', form=form)
 
-# In this function we split text into sentences, display them open
-# the screen and allow user to choose the "main" sentence for sorting.
+
 @app.route('/choose')
 def choose():
+    """
+    In this function we split text into sentences, display them open
+    the screen and allow user to choose the "main" sentence for sorting.
+    """
     global _text
     text = ''.join(_text)
-    #text = ''.join(get_flashed_messages()[0])
     sentences = split_into_sentences(text)
-    #flash(sentences)
-    return render_template('choose.html', sentences = sentences)
+    return render_template('choose.html', sentences=sentences)
 
-# Sorting sentences using our function from models.py file
-# and finally displaying them on the screen in the right order.
+
 @app.route('/unswer/<sent>')
 def unswer(sent):
+    """
+    Sorting sentences using our function from models.py file
+    and finally displaying them on the screen in the right order.
+    """
     global _text
     text = _text
-    #text = ''.join(get_flashed_messages()[0])
     sentences = split_into_sentences(text)
-    sorted = get_sorted_messages(sentences,sent)
-    return render_template('unswer.html', sentences = sorted)
+    sorted = get_sorted_messages(sentences, sent)
+    return render_template('unswer.html', sentences=sorted)
 
-# Starting the Flask app
+
 if __name__ == '__main__':
+    """Starting the Flask app"""
     app.run()
--- models.py
+++ models.py
@@ -4,31 +4,34 @@ from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 import gensim
 
-# Form that is used to get input text from the user
+
 class InForm(FlaskForm):
+    """Form that is used to get input text from the user."""
     text = TextAreaField('Text', validators=[DataRequired()])
     submit = SubmitField('Send')
 
-# Function for spliting text into sentences using re library.
+
 def split_into_sentences(text):
+    """Function for spliting text into sentences using re library."""
     caps = "([A-Z])"
     prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
     suffixes = "(Inc|Ltd|Jr|Sr|Co)"
-    starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+    starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s\
+    |Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
     acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
     websites = "[.](com|net|org|io|gov)"
     text = " " + text + "  "
-    text = text.replace("\n"," ")
-    text = re.sub(prefixes,"\\1<prd>",text)
-    text = re.sub(websites,"<prd>\\1",text)
-    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
-    text = re.sub("\s" + caps + "[.] "," \\1<prd> ",text)
-    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = text.replace("\n", " ")
+    text = re.sub(prefixes, "\\1<prd>", text)
+    text = re.sub(websites, "<prd>\\1", text)
+    if "Ph.D" in text: text = text.replace("Ph.D.", "Ph<prd>D<prd>")
+    text = re.sub("\s" + caps + "[.] "," \\1<prd> ", text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2", text)
     text = re.sub(caps + "[.]" + caps + "[.]" + caps + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
-    text = re.sub(caps + "[.]" + caps + "[.]","\\1<prd>\\2<prd>",text)
-    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
-    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
-    text = re.sub(" " + caps + "[.]"," \\1<prd>",text)
+    text = re.sub(caps + "[.]" + caps + "[.]", "\\1<prd>\\2<prd>", text)
+    text = re.sub(" "+suffixes+"[.] "+starters, " \\1<stop> \\2", text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>", text)
+    text = re.sub(" " + caps + "[.]"," \\1<prd>", text)
     if "\"" in text: text = text.replace(".\"","\".")
     if "!" in text: text = text.replace("!\"","\"!")
     if "?" in text: text = text.replace("?\"","\"?")
@@ -41,29 +44,35 @@ def split_into_sentences(text):
     sentences = [s.strip() for s in sentences]
     return sentences
 
-# Simple function to make code more compact, used only in
-# "get_sorted_messages" function.
+
 def split_to_words(sentences):
+    """
+    Simple function to make code more compact, used only in
+    "get_sorted_messages" function.
+    """
     words = []
     for sent in sentences:
         words.append(sent.split())
     return words
 
-# Function that is sorting sentences (first parameter) in order
-#  of similarity to the given sentence (second parameter).
-# Based on the gensim library, returns a list of sorted sentences.
+
 def get_sorted_messages(sentences, sent):
-    numOfMainSent = sentences.index(sent)
+    """
+    Function that is sorting sentences (first parameter) in order
+    of similarity to the given sentence (second parameter).
+    Based on the gensim library, returns a list of sorted sentences.
+    """
+    number_of_main_sentence = sentences.index(sent)
     words = split_to_words(sentences)
     similarity = []
     model = gensim.models.Word2Vec(words)
     for i in range(len(sentences)-1):
         k = 0
-        wordsOfSent = sentences[i].split()
-        for word in sentences[numOfMainSent]:
-            for w in wordsOfSent:
+        words_of_sentence = sentences[i].split()
+        for word in sentences[number_of_main_sentence]:
+            for w in words_of_sentence:
                 try:
-                    k += abs(model.similarity(word,w))
+                    k += abs(model.similarity(word, w))
                 except Exception:
                     pass
         similarity.append(k)
